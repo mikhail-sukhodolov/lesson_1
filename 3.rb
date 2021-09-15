1850f78218750fb461b3eb9e502cafdfa0e24e10
mikhail-sukhodolov <82030194+mikhail-sukhodolov@users.noreply.github.com>
2021-09-15 15:38:23 +0300

    Create 3.rb

---
+++ 3.rb
@@ -0,0 +1,11 @@
+puts "Please input the length of the three sides of the triangle - x, y, z"
+x, y, z = gets.to_i, gets.to_i, gets.to_i
+max = [x, y, z].max
+
+if x == y && y == z
+  puts "your the triangle is isosceles and equilateral, but not rectangular"
+elsif (max**2)*2 == x**2 + y**2 + z**2
+  puts "your the triangle is rectangular"
+elsif x == y || y == z || x == z 
+  puts "your the triangle is isosceles"
+end
