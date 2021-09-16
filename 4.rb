e40b86f15b52513733f7136f0f517111477844cf
mikhail-sukhodolov <82030194+mikhail-sukhodolov@users.noreply.github.com>
2021-09-16 07:07:16 +0300

    Update 4.rb

--- 4.rb
+++ 4.rb
@@ -1,11 +1,11 @@
 puts "Please input three coefficients a, b, c"
 a, b, c = gets.to_i, gets.to_i, gets.to_i
-d = b**2 - 4*a*c
+d = b**2 - 4 * a * c
 
 if d < 0
   puts "no square roots"
 elsif d == 0
-  puts "D = #{d}, X = #{(-b + Math.sqrt(d))/(2*a)}
+  puts "D = #{d}, X = #{(-b + Math.sqrt(d))/(2 * a)}
 else
-   puts "D = #{d}, X1 = #{(-b + Math.sqrt(d))/(2*a), X2 = #{(-b + Math.sqrt(d))/(2*a)}
+   puts "D = #{d}, X1 = #{(-b + Math.sqrt(d))/(2 * a), X2 = #{(-b + Math.sqrt(d))/(2 * a)}
 end
